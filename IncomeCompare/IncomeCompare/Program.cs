9f874d21913d01e1a8c829294aefd58827a49c3a
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-06-27 21:53:32 -0700

    IncomeCompare Assignment

---
+++ IncomeCompare/IncomeCompare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeCompare
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1, what is your hourly rate?");
+            decimal rateOne = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Person 1, how many hours per week do you work?");
+            decimal hoursOne = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Person 2, what is your hourly rate?");
+            decimal rateTwo = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Person 2, how many hours per week do you work?");
+            decimal hoursTwo = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Annual Salary of Person One:");
+            Console.WriteLine(rateOne * hoursOne * 52);
+            Console.WriteLine("Annual Salary of Person Two:");
+            Console.WriteLine(rateTwo * hoursTwo * 52);
+            Console.ReadLine();
+
+        }
+    }
+}
