f6f5487f4ace176011886c8cb2f2de0d7a3a0d77
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-06-27 23:52:17 -0700

    add loops

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number between 1 and 4:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 4;
+            do
+            {
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You are correct.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.Write("You are wrong.");
+                        Console.Write("Guess a number between 1 and 4:");
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+        }
+    }
+}
---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 4:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 4;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Try Again.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Try Again.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("Try Again.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You are correct.");
+                        break;
+                    default:
+                        Console.WriteLine("Try Again.");
+                        Console.WriteLine("Guess a number between 1 and 4:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
