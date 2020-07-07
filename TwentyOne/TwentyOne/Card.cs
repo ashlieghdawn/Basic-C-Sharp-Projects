89424fc8ef01433095eb893e839e7bb5d5a169b7
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-06 19:22:28 -0700

    add TwentyOne Assignment

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }  //making this property public makes it accessible to other parts of the program
+        public string Face { get; set; }
+    }
+}
