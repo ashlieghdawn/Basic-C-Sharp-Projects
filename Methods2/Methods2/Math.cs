aea9caf839d472cafc8d5d0f53a492f3ebeaa356
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-06 20:29:20 -0700

    Methods2

---
+++ Methods2/Methods2/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Math
+    {
+        public int Add(int a)
+        {
+            int result = a + 4;
+            return result;
+        }
+
+        public decimal Add(decimal b)
+        {
+            decimal result = Decimal.ToInt32(b*4);
+            return result;
+        }
+
+        public void Add(string c)
+        {
+            int len = c.Length;
+            int num = len + 44;
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math op = new Math();
+
+            Console.WriteLine("Please enter a whole number: ");
+            int mathA = op.Add(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Please enter a decimal number: ");
+            decimal mathB = op.Add(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine("Please enter another number: ");
+            string mathC = Console.ReadLine();
+            op.Add(mathC);
+
+            Console.WriteLine(mathA);
+            Console.WriteLine(mathB);
+            Console.WriteLine(mathC);
+            Console.ReadLine();
+
+
+        }
+    }
+}
