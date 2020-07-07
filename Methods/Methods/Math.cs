1321dccc2b983e69b66c0f6fb65968487b4a33b1
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-06 19:32:43 -0700

    add Method assignment

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Math
+    {
+        public int triple;
+        public int quadruple;
+        public int square;
+
+        public int tripleNumber(int userNum)
+        {
+            triple = 3 * userNum;
+            return triple;
+        }
+
+        public int quadrupleNumber(int userNum)
+        {
+            quadruple = 4 * userNum;
+            return quadruple;
+        }
+
+        public int squareNumber(int userNum)
+        {
+            square = userNum * userNum;
+            return square;
+        }
+        
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+            int variableTriple = math.tripleNumber(userNum);
+            int variableQuadruple = math.quadrupleNumber(userNum);
+            int variableSquare = math.squareNumber(userNum);
+
+            Console.WriteLine("Triple of that number is " + variableTriple);
+            Console.WriteLine("Quadruple of that number is " + variableQuadruple);
+            Console.WriteLine("The square of that number is " + variableSquare);
+            Console.ReadLine();
+        }
+    }
+}
