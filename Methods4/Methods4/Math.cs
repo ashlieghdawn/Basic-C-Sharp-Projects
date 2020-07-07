ba74b76cf5525170e48c3ed25f1955629e4d92e7
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-06 20:52:28 -0700

    Methods4

---
+++ Methods4/Methods4/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    public class Math
+    {
+        public static void ModifyValue(int i, int x)
+        {
+            i = i * 3;
+            Console.WriteLine("Your first number was tripled to = {0}", i);
+            Console.WriteLine("Your second number stayed the same = {0}", i);
+            return;
+        }
+        
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            int num2 = Convert.ToInt32(Convert.ToInt32(Console.ReadLine()));
+
+            Math.ModifyValue(num1, num2);
+            Math.ModifyValue(i: num1, x: num2);
+            Console.ReadLine();
+        }
+    }
+}
