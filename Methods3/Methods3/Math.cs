ac28fe3cdbdd9a4f87c8253d05330cd289cb547c
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-06 20:39:06 -0700

    Add Methods3

---
+++ Methods3/Methods3/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    public class Math
+    {
+        public void Addition(int a, int b = 0)
+        {
+            int result = a + b;
+            Console.WriteLine("First Number: {0}", a);
+            Console.WriteLine("Second Number: {0}", b);
+            Console.WriteLine("Add up to be: {0}", result);
+        }
+    }
+}
---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math op = new Math();
+            Console.WriteLine("Please enter a number: ");
+            int numA = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("Please enter another number {optional} or type x to continue:");
+                int numB = Convert.ToInt32(Console.ReadLine());
+                op.Addition(numA, numB);
+            }
+            catch(Exception ex)
+            {
+                op.Addition(numA);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
