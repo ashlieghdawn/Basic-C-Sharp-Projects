f579b488bba12bb612047ba3343cbac26cbcca48
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-07 19:04:47 -0700

    add overload

---
+++ Overload/Overload/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Id = 1;
+
+            Employee employee2 = new Employee();
+            employee2.Id = 2;
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 != employee2);
+            Console.ReadLine();
+        }
+    }
+}
