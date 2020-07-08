1538276ab8da7d341f3be8cd819cb88f58cd83e6
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-07 17:21:56 -0700

    add abstract

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public class Employee : Person
+    {
+        public int id { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.id = 1;
+            person.SayName();
+
+            
+              
+        }
+    }
+}
