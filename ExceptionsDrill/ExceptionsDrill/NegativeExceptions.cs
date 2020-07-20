fe4966a83afb8250f7f4ba6bea406e6e78936394
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-20 08:42:34 -0700

    add exception drill

---
+++ ExceptionsDrill/ExceptionsDrill/NegativeExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsDrill
+{
+    public class NegativeExceptions
+    {
+        public NegativeExceptions()
+            : base() { }
+        public NegativeExceptions(string message)
+            : base(message) { }
+    }
+}
---
+++ ExceptionsDrill/ExceptionsDrill/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsDrill
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge < 0)
+                {
+                    throw new NegativeException();
+                }
+                if (userAge == 0)
+                {
+                    throw new ZeroException();
+                }
+                int born = 2020 - userAge;
+                Console.WriteLine("You were born in {0}", born);
+                Console.ReadLine();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("That was invalid");
+                Console.ReadLine();
+            }
+            catch(NegativeException)
+            {
+                Console.WriteLine("Ages cannot be in negatives numbers.  It is impossible.");
+                Console.ReadLine();
+            }
+            catch(ZeroException)
+            {
+                Console.WriteLine("You are not 0 years old.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
