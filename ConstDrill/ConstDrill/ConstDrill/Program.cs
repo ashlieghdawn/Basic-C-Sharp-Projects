491489b37d545095b49a43ff3abe262828de310c
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-07-20 08:19:18 -0700

    add drill

---
+++ ConstDrill/ConstDrill/ConstDrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string currentYear = "2020";
+            Console.WriteLine("We are in the year {0}", currentYear);
+            Console.ReadLine();
+
+            var yearEvents = new { Events = "Corona Virus, Killers Bees, Racial Justice Protests", Causes = "guidelines not being followed and injustice in our justice system"};
+            Console.WriteLine("2020 Events\n" + yearEvents);
+            Console.ReadLine();
+        }
+    }
+}
